b44ed2e8e2aac876de837fd3e2b5ac43168e7ccf
Ivaylo <ivaylo.kuzmanov@gmail.com>
2018-05-25 23:54:18 +0300

    blog project launched

--- blog/forms.py
+++ blog/forms.py
@@ -1,22 +1,27 @@
 from django import forms
-from blog.models import Comment, Post
+
+from .models import Post, Comment
+
 
 class PostForm(forms.ModelForm):
 
-    class Meta():
+    class Meta:
         model = Post
-        fields = ('author','title','text')
+        fields = ('author','title', 'text',)
+
         widgets = {
-            'title':forms.TextInput(attrs={'class':'textinput'}),
-            'text':forms.TextArea(attrs={'class':'editable medium-editor-textarea postcontent'}),
+            'title': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'}),
         }
+
 
 class CommentForm(forms.ModelForm):
 
-    class Meta():
+    class Meta:
         model = Comment
-        fields = ('author','text')
+        fields = ('author', 'text',)
+
         widgets = {
-            'author':forms.TextInput(attrs={'class':'textinput'}),
-            'text':forms.TextArea(attrs={'class':'editable medium-editor-textarea'}),
+            'author': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'}),
         }
--- demos/urls.py
+++ demos/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', include('todo.urls')),
     path('oms/', include('oms.urls')),
     path('blog/', include('blog.urls')),
+    path('accounts/login/', views.login, name='login'),
+    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
