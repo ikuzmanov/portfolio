f32bffec3ecbdfd66b354db27bf3f94eb31173a0
Ivaylo <ivaylo.kuzmanov@gmail.com>
2018-05-26 23:09:02 +0300

    blog update - main page

--- blog/models.py
+++ blog/models.py
@@ -20,11 +20,8 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("post_detail",kwargs={'pk':self.pk})
 
-
     def __str__(self):
         return self.title
-
-
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments',on_delete=models.CASCADE)
